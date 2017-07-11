56111901f713e604d153be4b942e097487a14db4
Jorge Parra <jparra@trabajando.com>
2017-07-11 18:54:35 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,34 +7,39 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { MapPage } from '../pages/map/map';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        AboutPage,
+        ContactPage,
+        HomePage,
+        TabsPage,
+        MapPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        AboutPage,
+        ContactPage,
+        HomePage,
+        TabsPage,
+        MapPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        Geolocation,
+        { provide: ErrorHandler, useClass: IonicErrorHandler }
+    ]
 })
 export class AppModule {}
---
+++ src/pages/map/map.ts
@@ -0,0 +1,64 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google;
+
+@Component({
+    selector: 'page-map',
+    templateUrl: 'map.html'
+})
+export class MapPage {
+
+    @ViewChild('map') mapElement: ElementRef;
+    map: any;
+
+    constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+
+    }
+
+    ionViewDidLoad() {
+        console.log('aca');
+        this.loadMap();
+    }
+
+    loadMap() {
+
+        this.geolocation.getCurrentPosition().then((position) => {
+
+            let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            let mapOptions = {
+                center: latLng,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+
+            var triangleCoords = [
+	            { lat: -33.4138893, lng: -70.6033061 },
+	            { lat: -33.4122704, lng: -70.592576 },
+	            { lat: -33.4122703, lng: -70.592576 },
+	            { lat: -33.4232356, lng: -70.5844158 }
+	        ];
+
+	        // Construct the polygon.
+	        var bermudaTriangle = new google.maps.Polygon({
+	            paths: triangleCoords,
+	            strokeColor: '#FF0000',
+	            strokeOpacity: 0.8,
+	            strokeWeight: 2,
+	            fillColor: '#FF0000',
+	            fillOpacity: 0.35
+	        });
+	        
+            this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+	        
+	        bermudaTriangle.setMap(this.map);
+
+
+        }, (err) => {
+            console.log(err);
+        });      
+    }
+
+}
